c385d2c7426526b90d992af5e2c0f11a52defefc
Mạnh Nguyễn <toilamanh16@gmail.com>
2026-08-31 17:34:03 +0700

    Code Java

--- J01014 - ƯỚC SỐ NGUYÊN TỐ LỚN NHẤT.java
+++ J01014 - ƯỚC SỐ NGUYÊN TỐ LỚN NHẤT.java
@@ -4,38 +4,25 @@
 import java.util.Scanner;
 
 public class Main {
-    private static boolean snt(long n) {
-        if (n < 2)
-            return false;
-        for (long i = 2; i * i <= n; i++) {
-            if (n % i == 0)
-                return false;
-        }
-        return true;
-    }
-
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        while (t-- > 0) {
+        while(t-->0){
+            long max = 0;
             long n = sc.nextLong();
-            long res = 0;
-            while (n % 2 == 0) {
-                res = 2;
-                n /= 2;
-            }
-            for (long i = 3; i * i <= n; i++) {
-                if (n % i == 0) {
-                    while (n % i == 0) {
-                        res = i;
-                        n /= i;
+            for(int i = 2; i <= Math.sqrt(n); i++) {
+                if(n % i == 0) {
+                    max = i;
+                    while(n % i == 0) {
+                        n/=i;
                     }
                 }
             }
-            if (n > 1)
-                res = n;
-
-            System.out.println(res);
+            if(n > 1) {
+                max = n;
+            }
+            System.out.println(max);
         }
         sc.close();
     }
