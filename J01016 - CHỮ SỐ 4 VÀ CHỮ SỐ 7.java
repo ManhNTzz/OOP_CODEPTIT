95c429b65acda91f190c0329d1806aac7b1261a3
Mạnh Nguyễn <toilamanh16@gmail.com>
2026-08-31 16:56:57 +0700

    Code Java

--- J01016 - CHỮ SỐ 4 VÀ CHỮ SỐ 7.java
+++ J01016 - CHỮ SỐ 4 VÀ CHỮ SỐ 7.java
@@ -1,25 +1,20 @@
 
-// cre by ManhNTzz
+// Cre by ManhNTzz
 
 import java.util.Scanner;
 
 public class Main {
-
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
-        char[] a = s.toCharArray();
-        int dem4 = 0, dem7 = 0;
-        for (char c : a) {
-            if (c == '4')
-                dem4++;
-            else if (c == '7')
-                dem7++;
+        int dem = 0;
+        for (char c:s.toCharArray()){
+            if(c == '4' || c == '7'){
+                dem++;
+            }
         }
-        if (dem4 + dem7 == 4 || dem4 + dem7 == 7) {
-            System.out.println("YES");
-        } else
-            System.out.println("NO");
+        System.out.println((dem == 4 || dem == 7) ? "YES" : "NO");
         sc.close();
     }
 }
